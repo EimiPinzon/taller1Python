551aef827fe3d274084edf43826020680947acb0
Eimi Pinzón <eimipinzonhoyos@gmail.com>
2020-08-16 16:00:47 -0500

    Taller 2 de python

---
+++ pythonProject2/taller2.py
@@ -0,0 +1,55 @@
+#Problemas condicionales
+#1
+
+'''intereses=float(input("Digite sus intereses"))
+capital=float(input("Digite su capital"))
+inte=capital*intereses
+
+if inte>7000:
+    print("Invertir intereses", inte)
+else:
+    print("No invertir intereses")
+
+cap_f=capital+inte
+
+print("El dinero que tiene en su cuenta es de", cap_f)
+
+#2
+cali_1=float(input("Digite el valor de su calificación 1"))
+cali_2=float(input("Digite el valor de su calificación 2"))
+cali_3=float(input("Digite el valor de su calificación 3"))
+prom_cali=(cali_1+cali_2+cali_3)/3
+if prom_cali >=70:
+    print("Alumno aprobado")
+else:
+    print("Alumno reprobado")
+
+#3
+val_compra=float(input("Digite el valor de su compra"))
+if val_compra>1000:
+    desc=val_compra*0.20
+else:
+    desc=0
+
+total_pag=val_compra-desc
+print("El valor de su compra en total es de:", total_pag)
+
+#4
+horas_traba=float(input("Digite el número de horas trabajadas"))
+if horas_traba<40:
+    sal_h=horas_traba-40
+    ss=sal_h*20+40*16
+else:
+    ss=horas_traba*16
+
+    print("Su salario es de:", ss)
+
+#5
+num1=float(input("Digite su primer número"))
+num2=float(input("Digite su segundo número"))
+if num1<num2:
+    print( num1, num2)
+else:
+    print(num2, num1)'''
+
+
